a2d3d7ca8501cafb0f54730218dace81dc2f1a61
PleSo <alexandru@mgesoftware.com>
2022-01-26 04:21:06 +0200

    updated setup.cfg

--- setup.py
+++ setup.py
@@ -1,16 +1,31 @@
 from setuptools import setup, find_packages
 
-
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+    
 setup(
     name='mge_graphql',
-    version='1.0',
+    version='1.0.1',
     license='BSD 3-Clause "New" or "Revised"',
     author="Alexandru Plesoiu",
     author_email='alexandru@mgesoftware.com',
+    description="GraphQL support with data validations, error handle and permission support built on top of graphene.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=find_packages(exclude=["examples*"]),
     url='https://github.com/mgesoftware/mge-graphql',
-    keywords='graphene mongodb flask graphql mge_graphql',
+    keywords='api graphene mongodb flask graphql mge_graphql rest relay',
     install_requires=[
           'graphene',
       ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ]
 )
