af82cadca78ba7050700d9674c45d95056de49be
PleSo <alexandru@mgesoftware.com>
2022-01-26 05:38:25 +0200

    Added Graphene error objecttype

---
+++ mge_graphql/types/common.py
@@ -0,0 +1,14 @@
+import graphene
+
+class Error(graphene.ObjectType):
+    field = graphene.String(
+        description=(
+            "Name of a field that caused the error. A value of `null` indicates that "
+            "the error isn't associated with a particular field."
+        ),
+        required=False,
+    )
+    message = graphene.String(description="The error message.")
+
+    class Meta:
+        description = "Represents an error in the input of a mutation."
